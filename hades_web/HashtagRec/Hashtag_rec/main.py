8939f9242210ddf77ab6d37417fa7c3d1466b20b
C.E.Huang <zero90169@hotmail.com>
2019-06-01 00:31:55 +0800

    testing part over

--- hades_web/HashtagRec/Hashtag_rec/main.py
+++ hades_web/HashtagRec/Hashtag_rec/main.py
@@ -34,7 +34,7 @@ def command():
     parser.add_argument("-bs", "--batch_size", type=int, dest="bs",
                         help="Batch_size of network, Default is 64", default=64)
     parser.add_argument("-epos", "--epochs", type=int, dest="epos",
-                        help="Numbers of epochs, Default is 200", default=100)
+                        help="Numbers of epochs, Default is 100", default=100)
     parser.add_argument("-s", "--save", type=str, dest="save",
                         help="Path of training model, default is model/", default="model/")
     parser.add_argument("-th", "--threshold", type=int, dest="th",
@@ -46,38 +46,74 @@ def command():
     parser.add_argument("-end2end", type=str, dest="end2end", choices=['True', 'False'], default="False")
     return parser
 
-def select_model(args, word2idx):
-    use_e2e = True if args.end2end == "True" else False
+def select_model(args, word2idx, use_e2e):
     if args.mt == 'devise':
-        core_model = devise(end2end=True) if use_e2e else devise()
+        core_model = devise(end2end=use_e2e)
         criterion = nn.MSELoss()
     elif args.mt == 'multi_label':
-        core_model = multi_label(len(word2idx), end2end=True) if use_e2e else multi_label(len(word2idx))
+        core_model = multi_label(len(word2idx), end2end=use_e2e)
         criterion = nn.BCELoss()
     elif args.mt == 'dnn':
-        core_model = dnn(len(word2idx), end2end=True) if use_e2e else dnn(len(word2idx))
+        core_model = dnn(len(word2idx), end2end=use_e2e)
         criterion = nn.NLLLoss()
     return core_model, criterion
 
-def testing(image_model, core_model, word2idx, word_vec_dict, vec_matrix):
+def testing(args, core_model, word2idx, word_vec_dict, vec_matrix, end2end):
+    from src.train import handle_output_format
     fix_path = "/home/chihen/NTU/Research/Hashtag/HARRISON/"
     print("You should enter to list element separated by space and follow the format: <usr> <img.jpg> <YYMMDDHHmmss>")
-    crop = img_centre_crop()
+
     input_string = input()
     info = input_string.split()
+    tmp = info.copy()
     
     tmp_path = os.path.join(fix_path, info[1])
     img = Image.open(tmp_path).convert('RGB')
+    crop = img_centre_crop()
     input_img = crop(img).unsqueeze(0).to(device)
-
-    tmp = info.copy()
     with torch.no_grad():
-        img_feat = image_model(input_img)
-        pred = core_model(img_feat.squeeze().unsqueeze(0)).detach().cpu()
+        if not end2end:
+            import torchvision.models as models
+            resnet50 = models.resnet50(num_classes=1000, pretrained=True).eval()
+            modules = list(resnet50.children())[:-1]
+            resnet50 = nn.Sequential(*modules).to(device)
+            input_img = resnet50(input_img)
+            input_img = input_img.view(1, 1, -1)
+        pred = core_model(input_img)
+        pred = handle_output_format(args, pred, word_vec_dict)
+        top_k_result = top_k(pred, vec_matrix, 10)[0] if args.mt != "multi_label" else pred[0]
+        for ele in top_k_result:
+            tmp += [idx2word[ele]]
+        print(tmp)
+
+def heracles_testing(args, heracles, word2idx, word_vec_dict, vec_matrix, end2end):
+    from src.train import handle_output_format
+    fix_path = "/home/chihen/NTU/Research/Hashtag/HARRISON/"
+    print("You should enter to list element separated by space and follow the format: <usr> <img.jpg> <YYMMDDHHmmss>")
+
+    input_string = input()
+    info = input_string.split()
+    tmp = info.copy()
+    
+    tmp_path = os.path.join(fix_path, info[1])
+    img = Image.open(tmp_path).convert('RGB')
+    crop = img_centre_crop()
+    input_img = crop(img).unsqueeze(0).to(device)
+    with torch.no_grad():
+        if not end2end:
+            import torchvision.models as models
+            vgg19bn = models.vgg19_bn(num_classes=1000, pretrained=True).eval()
+            modules = list(vgg19bn.children())[:-1]
+            vgg19bn = nn.Sequential(*modules).to(device)
+            input_img = vgg19bn(input_img)
+            # input_img = input_img.view(1, 1, -1)
+        pred = heracles(input_img)
+        pred = handle_output_format(args, pred, word_vec_dict)
         top_k_result = top_k(pred, vec_matrix, 10)[0]
         for ele in top_k_result:
             tmp += [idx2word[ele]]
         print(tmp)
+
 
 if __name__ == "__main__":
     folder_dir = "/home/chihen/NTU/Research/Hashtag/HARRISON/"
@@ -103,27 +139,39 @@ if __name__ == "__main__":
     # word2idx = word_vec['dictionary'].tolist()
     # idx2word = word_vec['reverse_dictionary'].tolist()
     ####################
-    if args.mt != 'heracles':
-        core_model, loss_fn = select_model(args, word2idx)
-        core_model = core_model.to(device)
-        print(core_model)
-    else:
-        heracles = Heracles().to(device)
-        labours = Labours().to(device)
-        loss_fn = nn.MSELoss()
-        print(heracles)
-        print(labours)
 
+    use_e2e = True if args.end2end == "True" else False
     if args.mode == "train":
         if args.mt != 'heracles':
+            core_model, loss_fn = select_model(args, word2idx, use_e2e)
+            core_model = core_model.to(device)
+            print(core_model)
             training(args, core_model, folder_dir, word2idx, idx2word, word_vec_dict, vec_matrix, loss_fn)
         else:
+            heracles = Heracles().to(device)
+            labours = Labours().to(device)
+            loss_fn = nn.MSELoss()
+            print(heracles)
+            print(labours)
             train_heracles(args, heracles, labours, folder_dir, word2idx, idx2word, word_vec_dict, vec_matrix, loss_fn)
     else:
-        if args.mt == "devise":
-            m_path = os.path.join(args.save, 'best_val')
-            core_model.load_state_dict(torch.load(m_path+'_{}.pth'.format(args.mt)))
+        if args.mt != "heracles":
+            m_type = 'E2E' if use_e2e else 'nonE2E'
+            m_path = os.path.join(args.save, 'best_f1_val_{}_{}.pth'.format(m_type, args.mt))
+            
+            core_model, _ = select_model(args, word2idx, use_e2e)
+            print(m_path)
+            core_model.load_state_dict(torch.load(m_path))
+            core_model = core_model.to(device)
             core_model.eval()
             while True:
-                pass
-                # testing(image_model, core_model, word2idx, word_vec_dict, vec_matrix)
+                testing(args, core_model, word2idx, word_vec_dict, vec_matrix, end2end=use_e2e)
+        else:
+            m_path = os.path.join(args.save, 'best_f1_val_False_H_heracles.pth')
+            heracles = Heracles()
+            heracles.load_state_dict(torch.load(m_path))
+            heracles = heracles.to(device)
+            heracles.eval()
+            while True:
+                heracles_testing(args, heracles, word2idx, word_vec_dict, vec_matrix, end2end=use_e2e)
+
